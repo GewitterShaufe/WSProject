107ac71e17bc62d376ee3a9d7a7d043957c09a17
AJIbfa <Super.ajibfa@yandex.ru>
2017-02-12 12:28:37 +0300

    Updated Index And Created model Weapon

---
+++ WebProject/Models/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class Weapon
+    {
+
+    public int Id { get; set; }
+
+    }
+}
