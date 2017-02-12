34c527cbd2ff07619897e7f3bad660e71ba3ab9d
RusVoin <loba4off@yandex.ru>
2017-02-12 12:58:55 +0300

    Тест от Саши

--- WebProject/Global.asax.cs
+++ WebProject/Global.asax.cs
@@ -16,6 +16,7 @@ namespace WebProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //рас рас
         }
     }
 }
